03f96bba81186f75723c54d320290b379be4d109
guillaume baelen <guillaume@proctorexam.com>
2018-08-05 11:50:52 +0200

    Added pods list gathering

--- lib/kubectl_rails_runner.rb
+++ lib/kubectl_rails_runner.rb
@@ -1,5 +1,22 @@
 class KubectlRailsRunner
+  attr_accessor :namespace
+  attr_accessor :pod
+
+  def initialize(namespace = "namespace", pod = "pod")
+    @namespace = namespace
+    @pod       = pod
+  end
+
+  def self.get_pods
+    pods       = `kubectl get pods --all-namespaces`
+    pods_array = pods.split("\n").map do |s| 
+      s.gsub(/\s+/m, ' ').strip.split(" ")
+    end
+    pods_array.shift
+    pods_array
+  end
+
   def self.run_command
-    puts "enter the command here"
+    p `kubectl --namespace=webrtc exec -it frontend-1765095596-dt5lf -c webapp -- rails runner 'p StudentSession.last'`
   end
 end
