18954f3cfbd875b69d8a9d551713fe7fc0190732
guillaume baelen <guillaume@proctorexam.com>
2018-09-10 00:59:15 +0200

    Added get_pod function by namespace

--- lib/kubectl_rails_runner.rb
+++ lib/kubectl_rails_runner.rb
@@ -7,7 +7,16 @@ class KubectlRailsRunner
     @pod       = pod
   end
 
-  def self.get_pods
+  def self.get_pods(namespace)
+    pods       = `kubectl get pods --namespace=#{@namespace}`
+    pods_array = pods.split("\n").map do |s| 
+      s.gsub(/\s+/m, ' ').strip.split(" ")
+    end
+    pods_array.shift
+    pods_array
+  end
+
+  def self.get_all_pods
     pods       = `kubectl get pods --all-namespaces`
     pods_array = pods.split("\n").map do |s| 
       s.gsub(/\s+/m, ' ').strip.split(" ")
@@ -16,7 +25,11 @@ class KubectlRailsRunner
     pods_array
   end
 
-  def self.run_command
-    p `kubectl --namespace=webrtc exec -it frontend-1765095596-dt5lf -c webapp -- rails runner 'p StudentSession.last'`
+  def self.init(namespace, pod)
+    return KubectlRailsRunner.new(namespace, pod)
+  end
+
+  def run_command(ruby_code)
+    p `kubectl --namespace=#{self.namespace} exec -it #{self.pod} -c webapp -- rails runner '#{ruby_code}'`
   end
 end
